698c7e98dbae22b01ff49f03d928fb1cdebd63a6
bkyn <21976395+Bkyn@users.noreply.github.com>
2017-11-13 22:55:15 +0100

    research on timeline plot

--- concept_timeline.py
+++ concept_timeline.py
@@ -1,32 +1,39 @@
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime
 import matplotlib.dates as mdates
+import matplotlib.ticker as mticker
 import random
-from numpy import cumsum
-base = datetime.today()
-date_list = [base + timedelta(minutes=x) for x in range(0, 1000)]
+from numpy import cumsum, mean
+import hotrecorder as hr
+
+data_collec = hr.read_json('datacollec.json')
+
+date_list = [datetime.strptime(data['timestamp'],"%b %d %Y %H:%M:%S") for data in data_collec]
+
 spot = date_list[200]
-test = [random.randint(-100,100) for n in range(len(date_list))]
-test2 = [random.randint(-1000,1500) for n in range(len(date_list))]
-test = cumsum(test)
-test2 = cumsum(test2)
+
+ups = [mean(data['ups']) for data in data_collec]
+coms = [mean(data['coms']) for data in data_collec]
+
 idx = date_list.index(spot)
 fig,ax1 = plt.subplots(figsize=(15,2))
 
-ax1.plot(date_list[:idx+1],test[:idx+1],color='b')
-ax1.plot(date_list[idx:],test[idx:],color='b', alpha=0.1)
+ax1.plot(date_list[:idx+1],ups[:idx+1],color='#375869')
+ax1.plot(date_list[idx:],ups[idx:],color='#375869', alpha=0.1)
 #ax1.plot([spot,spot],[0,max(test)],'k-', color ='grey')
-ax1.set_ylabel('test', color='b')
-ax1.tick_params('y', colors='b')
+ax1.set_ylabel('ups', color='#375869')
+ax1.tick_params('y', colors='#375869')
+ax1.yaxis.set_major_locator(mticker.MultipleLocator(5000))
 
 
 ax2 = ax1.twinx()
-ax2.plot(date_list[:idx+1],test2[:idx+1],color='r')
-ax2.plot(date_list[idx:],test2[idx:],color='r', alpha=0.1)
-ax2.set_ylabel('test', color='r')
-ax2.tick_params('y', colors='r')
+ax2.plot(date_list[:idx+1],coms[:idx+1],color='#e1694e')
+ax2.plot(date_list[idx:],coms[idx:],color='#e1694e', alpha=0.1)
+ax2.set_ylabel('coms', color='#e1694e')
+ax2.tick_params('y', colors='#e1694e')
 
-ax2.xaxis.set_major_formatter(mdates.DateFormatter("%d %b\n%H:%M"))
-ax2.xaxis.set_major_locator(mdates.HourLocator()) 
+ax2.xaxis.set_major_locator(mdates.HourLocator())
+ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M\n%d %b"))
 
-plt.show()
+#plt.show()
+plt.savefig('plots/1_test.png', bbox_inches='tight')
